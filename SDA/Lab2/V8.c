5905e1e1ee34947da28761b1e7a90801f0b3b8e6
Takkapi <takkapi.off@gmail.com>
2026-03-16 06:55:44 +0200

    Pregatirea pentru Laboratorul nr. 2

---
+++ SDA/Lab2/V8.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/// Laboratorul nr. 2 la Structuri de Date si Algoritm
+/// Realizat de Smolenschi Petru, TI-252FR
+
+/// Sarcina lucrarii:
+/*
+Pentru tabloul unidimensional dat cu elemente de tip structura (conform variantelor) 
+sa se afiseze la ecran urmatorul meniu recursiv de optiuni:
+    1. Introducerea elementelor tabloului de la tastatura.
+    2. Afisarea elementelor tabloului la ecran.
+    3. Sortarea tabloului crescator/descrescator (2 tehnici de sortare la alegere).
+    0. Iesire din program.
+
+Sa se elaboreze funtiile pentru implementarea optiunilor meniului.
+
+! Varianta 8
+Structura Tablete cu campurile: tip(tableta, carte electronica, tableta grafica), producator,
+dimensiune ecran, sistem de operare, numar de nuclee, pret.
+*/
+
+int main() {
+    printf("We are cooked!...\n");
+    
+    return 1;
+}
